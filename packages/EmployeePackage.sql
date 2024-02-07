ea69d4f67f5dc150593b51ec6ef4876e310c5b1c
Jakub <240810@edu.p.lodz.pl>
2024-02-07 21:28:01 +0100

    employeepackage completed

--- packages/EmployeePackage.sql
+++ packages/EmployeePackage.sql
@@ -1,6 +1,5 @@
-drop package EmployeePackage;
+DROP PACKAGE EMPLOYEEPACKAGE;
 /
-
 CREATE OR REPLACE PACKAGE EmployeePackage AS
 
     PROCEDURE AddEmployeeToBranch(
@@ -8,8 +7,19 @@ CREATE OR REPLACE PACKAGE EmployeePackage AS
         idForBranch IN NUMBER
     );
 
+     PROCEDURE CheckIfEmployeeIsCurrentlyWorking(
+        employeeToCheck IN EMPLOYEE
+    );
+
+    PROCEDURE CheckIfEmployeeWorksInTheBranch(
+        employeeToCheck IN EMPLOYEE,
+        idForBranch IN NUMBER
+    );
+
 END EmployeePackage;
 /
+
+drop package EmployeePackage;
 
 CREATE OR REPLACE PACKAGE BODY EmployeePackage AS
 
@@ -35,8 +45,6 @@ CREATE OR REPLACE PACKAGE BODY EmployeePackage AS
 
         INSERT INTO EMPLOYEESTABLE VALUES (employeeToAdd);
 
-        COMMIT;
-
         UPDATE BranchTable b
         SET b.Employees = b.Employees MULTISET UNION ALL (
             SELECT CAST(MULTISET(
@@ -47,7 +55,8 @@ CREATE OR REPLACE PACKAGE BODY EmployeePackage AS
         )
         WHERE b.BranchId = idForBranch;
 
-
+        COMMIT;
+        DBMS_OUTPUT.PUT_LINE('Employee added successfully');
         EXCEPTION
             WHEN EMPLOYEE_EXIST_EXCEPTION THEN
                 RAISE_APPLICATION_ERROR(-20005, 'Employee exists');
@@ -57,7 +66,54 @@ CREATE OR REPLACE PACKAGE BODY EmployeePackage AS
     END AddEmployeeToBranch;
 
 
+    PROCEDURE CheckIfEmployeeIsCurrentlyWorking(
+        employeeToCheck IN EMPLOYEE
+    ) IS
+        currentContractEnd DATE;
+    BEGIN
+        SELECT Contract_End_Date
+        INTO currentContractEnd
+        FROM EMPLOYEESTABLE
+        WHERE EmployeeId = employeeToCheck.EmployeeId;
+
+        IF currentContractEnd IS NULL THEN
+            RAISE_APPLICATION_ERROR(-20005, 'Employee has no active contract.');
+        ELSIF currentContractEnd >= SYSDATE THEN
+            DBMS_OUTPUT.PUT_LINE('Employee is currently working.');
+        ELSE
+            RAISE_APPLICATION_ERROR(-20005, 'Employee contract has ended.');
+        END IF;
+    END CheckIfEmployeeIsCurrentlyWorking;
+
+    PROCEDURE CheckIfEmployeeWorksInTheBranch(
+        employeeToCheck IN EMPLOYEE,
+        idForBranch IN NUMBER
+    ) IS
+        isWorking NUMBER;
+    BEGIN
+         BEGIN
+            CheckIfEmployeeIsCurrentlyWorking(employeeToCheck);
+        EXCEPTION
+            WHEN OTHERS THEN
+                DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+                RETURN;
+        END;
+
+        SELECT COUNT(*)
+        INTO isWorking
+        FROM BranchTable b
+        WHERE b.BranchId = idForBranch AND EXISTS (
+            SELECT 1
+            FROM TABLE(b.Employees) e
+            WHERE e.PESEL = employeeToCheck.PESEL
+        );
+
+        IF isWorking > 0 THEN
+            DBMS_OUTPUT.PUT_LINE('Employee works in the specified branch.');
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('Employee does not work in the specified branch.');
+        END IF;
+    END CheckIfEmployeeWorksInTheBranch;
+
 END EmployeePackage;
 /
-
-select * from dual;
